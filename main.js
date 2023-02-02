26fd208eaf80c6e22b0b9455008d1b7ba85d5830
NicolasEiriz <eiriznicolas@gmail.com>
2023-02-02 20:11:12 -0300

    add three buttons to make a new gride, clear and give a random color

--- main.js
+++ main.js
@@ -4,37 +4,26 @@
 
 // It’s best to put your grid squares inside another “container” div (which can go directly in your HTML).
 
-// //Create the main container
-
-// let container = document.createElement('div')
-
-
-// //Append to the body
-
-// document.body.appendChild(container)
-
-// //Add the clase 'container' to give them some style
-
-// container.classList = 'container'
-
 
 //Create a row 16 times with 16 divs inside it
 
 function makeGrid(nums = 16){
 
+  //Create the main container
+
   let container = document.createElement('div')
 
 
-//Append to the body
+  //Append to the body
 
-document.body.appendChild(container)
+  document.body.appendChild(container)
 
-//Add the clase 'container' to give them some style
+  //Add the class 'container' to give them some style
 
-container.classList = 'container'
+  container.classList = 'container'
 
 
-for(let i = 0; i < nums; i++){
+  for(let i = 0; i < nums; i++){
   
   //Create a row and assigned the row class
   
@@ -46,58 +35,92 @@ for(let i = 0; i < nums; i++){
  
   //Create the divs inside the row
 
-  for(let j = 0; j < nums; j++){
+    for(let j = 0; j < nums; j++){
 
-    let box = document.createElement('div')
+      let box = document.createElement('div')
 
-    box.classList = 'divs'
+      box.classList = 'divs'
 
-    row.appendChild(box)
+      //insert the box in the row
 
-  }
-}
-}
+      row.appendChild(box)
 
-makeGrid()
+    }
+
+    let gridDivs = document.querySelectorAll('.divs')
+
+    //add an event listener to each one of them
+
+    gridDivs.forEach(div=>{
+      
+      div.addEventListener('mouseenter', (e)=>{
+    
+
+    //random color
+    e.target.style.backgroundColor = `white`
+  })
+})
+  }}
 
 //Set up a hover effect so the grid divs change the color when your mouse passes over them
 
 //Select all the grid divs -> this is a node list (similar to an array) so we can use forEach method
 
-let gridDivs = document.querySelectorAll('.divs')
 
-//add an event listener to each one of them
-
-gridDivs.forEach(div=>{
-  div.addEventListener('mouseenter', (e)=>{
-    e.target.style.backgroundColor = 'red'
-  })
-})
+makeGrid()
 
 
-//Add a button to the top of the screen
-
-let button = document.createElement('button')
-
-button.textContent = 'click'
-button.style.margin = '0 0 10px 0'
-
-document.body.appendChild(button)
 
 
+let button = document.querySelectorAll('button')
 
 
 //Send the user a popup asking for the number of squares per side for the new grid
 
 
-button.addEventListener('click', ()=>{
-  let userInput = Number(prompt('select a number of squares for the new grid'))
+button.forEach(btn=>{
 
-  //Once entered the existing grid should be removed and a new grid should be generated in the same total space as before
+  btn.addEventListener('click', (e)=>{  
 
-  document.querySelector('.container').remove()
-  makeGrid(userInput)
+    if(e.target.className === 'newGrid'){
 
+      let userInput = Number(prompt('select a number of squares for the new grid'))
 
-})
+      //Once entered the existing grid should be removed and a new grid should be generated in the same total space as before
+
+      document.querySelector('.container').remove()
+
+      makeGrid(userInput)
+    }
+    
+    else if(e.target.className === 'rainbow'){
+
+      //Select all the grid divs -> this is a node list (similar to an array) so we can use forEach method
+
+      let gridDivs = document.querySelectorAll('.divs')
+
+      //add an event listener to each one of them
+
+      gridDivs.forEach(div=>{
+
+        div.addEventListener('mouseenter', (e)=>{
+    
+        //random color
+        
+        e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*300)} ${Math.floor(Math.random()*300)} ${Math.floor(Math.random()*300)})`
+      
+        })
+      })
+
+    }
+
+      else if(e.target.className === 'clear'){
+
+        location.reload()
+      }
+
+  })
+}
+    )
+
 
